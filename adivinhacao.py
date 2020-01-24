154469d705aa2a68c41a55297eebab490499caaa
Rodrigo Dias <diasbass@hotmail.com>
2020-01-24 16:14:06 -0300

    testing conditional for with in range

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,16 +1,27 @@
 print('*************************')
-print('Python Test')
+print('Python Test - Número Secreto')
 print('*************************')
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-chute_str = input("Digite um número: ")
+for rodada in range(1, total_de_tentativas + 1):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute_str = input("Digite um número: ")
+    chute = int(chute_str)
+    print("Você digitou: ", chute)
 
-chute = int(chute_str)
+    acertou = chute == numero_secreto
+    chute_maior = chute > numero_secreto
+    chute_menor = chute < numero_secreto
 
-print("Você digitou: ", chute)
+    if (acertou):
+        print('Show! Você acertou o número secreto!')
+    else:
+        if(chute_maior):
+            print('Você errou! O seu número é maior que o número secreto.')
+        elif(chute_menor):
+            print('Você errou! O seu número é menor que o número secreto.')
 
-if (numero_secreto == chute):
-    print('Você acertou o número secreto')
-else:
-    print('você errou o número secreto')
+print("Fim do jogo!")
